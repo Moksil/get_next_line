ccb0981bd86c3cd034e26601375f5a9452de5daa
Sungmin <psm901@naver.com>
2022-01-21 15:36:29 +0900

    before_handin

--- get_next_line.c
+++ get_next_line.c
@@ -21,15 +21,16 @@ static void	file_node_free(t_list *lst, int fd)
 	cur = lst;
 	while (cur)
 	{
-		if (cur -> fd == fd)
+		if (cur->fd == fd)
 		{
 			if (prev)
-				prev -> next = cur -> next;
+				prev->next = cur->next;
 			free(cur);
+			cur = NULL;
 			break ;
 		}
 		prev = cur;
-		cur = cur -> next;
+		cur = cur->next;
 	}
 }
 
@@ -39,23 +40,25 @@ static t_list	*get_file_node(int fd, t_list *lst)
 	t_list			*cur;
 	t_list			*new_file;
 
+	if (fd > FD_MAX || fd < 0)
+		return (NULL);
 	prev = NULL;
 	cur = lst;
 	while (cur)
 	{
-		if (cur -> fd == fd)
+		if (cur->fd == fd)
 			return (cur);
 		prev = cur;
-		cur = cur -> next;
+		cur = cur->next;
 	}
 	new_file = (t_list *)malloc(sizeof(t_list));
 	if (new_file == NULL)
 		return (NULL);
 	if (prev)
-		prev -> next = new_file;
-	new_file -> fd = fd;
-	new_file -> remainder = NULL;
-	new_file -> next = NULL;
+		prev->next = new_file;
+	new_file->fd = fd;
+	new_file->remainder = NULL;
+	new_file->next = NULL;
 	return (new_file);
 }
 
@@ -63,33 +66,32 @@ static char	*get_line_from_remainder(t_list *file_node)
 {
 	char	*line;
 
-	if (file_node -> remainder == NULL)
+	if (file_node->remainder == NULL)
 		line = NULL;
 	else
 	{
-		line = ft_strndup(file_node -> remainder, -1);
+		line = ft_strndup(file_node->remainder, -1);
 		if (line == NULL)
 			return (NULL);
-		free(file_node -> remainder);
-		file_node -> remainder = NULL;
+		free(file_node->remainder);
+		file_node->remainder = NULL;
 	}
 	return (line);
 }
 
 static char	*read_and_remain(t_list *lst, char *line, int *flag, int fd)
 {
-	t_list	*s_file_list;
 	char	buf[BUFFER_SIZE + 1];
 	char	*nl_ptr;
+	char	*tmp;
 
-	s_file_list = get_file_node(fd, lst);
 	nl_ptr = ft_strchr(line, '\n');
 	while (nl_ptr == NULL)
 	{
 		*flag = read(fd, buf, BUFFER_SIZE);
 		if (*flag == 0 || *flag == -1)
 		{
-			if (*flag == -1 || (*flag == 0 && lst == s_file_list))
+			if (*flag == -1 || (*flag == 0 && lst == get_file_node(fd, lst)))
 				*flag -= 1;
 			else
 				*flag = 0;
@@ -99,9 +101,10 @@ static char	*read_and_remain(t_list *lst, char *line, int *flag, int fd)
 		line = ft_strjoin_m(line, buf);
 		nl_ptr = ft_strchr(line, '\n');
 	}
-	s_file_list -> remainder = ft_strndup(nl_ptr + 1, -1);
+	(get_file_node(fd, lst)->remainder) = ft_strndup(nl_ptr + 1, -1);
+	tmp = line;
 	line = ft_strndup(line, nl_ptr - line + 1);
-	free(nl_ptr - ft_strlen(line) + 1);
+	free(tmp);
 	return (line);
 }
 
@@ -112,8 +115,6 @@ char	*get_next_line(int fd)
 	char			*line;
 	int				flag;
 
-	if (fd > FD_MAX || fd < 0)
-		return (NULL);
 	file_node = get_file_node(fd, lst);
 	if (file_node == NULL)
 		return (NULL);
@@ -123,12 +124,14 @@ char	*get_next_line(int fd)
 	line = read_and_remain(lst, line, &flag, fd);
 	if (flag <= -1)
 	{
-		lst = file_node -> next;
+		lst = file_node->next;
 		free(file_node);
 		if (flag == -2)
 			return (NULL);
 	}
 	else if (flag == 0)
 		file_node_free(lst, fd);
+	if (!line && !(file_node->remainder))
+		return (NULL);
 	return (line);
 }
